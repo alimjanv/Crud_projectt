61a1ae94df68da1a2046729669468243bbb0dace
maralimjanv <umarolimjonov2009@gmail.com>
2024-11-15 20:41:22 +0500

    commit tur

--- Crud/book/views.py
+++ Crud/book/views.py
@@ -29,18 +29,18 @@ class BooksAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request):
-        books = Book.objects.all()
-        books.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def put(self, request):
-        books = Book.objects.all()
-        serializer = BookSerializers(books, data=request.data, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def delete(self, request):
+    #     books = Book.objects.all()
+    #     books.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+    #
+    # def put(self, request):
+    #     books = Book.objects.all()
+    #     serializer = BookSerializers(books, data=request.data, many=True)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
